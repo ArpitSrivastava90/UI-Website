e19bf22f2c6e98f9dca35e90a6bd21e51d6f1961
Arpit <arpitsrivastav2023@gmail.com>
2025-09-29 19:27:48 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="h-screen w-full bg-black px-10 py-10 flex justify-center items-center">
+      <div className="w-full h-full bg-[#1A1A1A] rounded-xl flex  flex-wrap justify-start items-start p-4 gap-2">
+        <div className="box w-60 h-80 bg-red-500 ml-5 rounded-xl"></div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
